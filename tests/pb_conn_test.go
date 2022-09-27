4081f7b6495c1cc3b929a462ec7d9165b80687fd
korman <korman@qq.com>
2022-09-28 00:22:37 +0800

    Added tests

---
+++ tests/pb_conn_test.go
@@ -0,0 +1,46 @@
+package tests
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func TestPb(t *testing.T) {
+	conn, err := net.DialTimeout("tcp", "127.0.0.1:9898", 5*time.Second)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i := 0; i < 1; i++ {
+		sayHello := &server.SayReq{
+			Text: "fff",
+		}
+
+		marshel, err := proto.Marshal(sayHello)
+
+		var lenth int = len(marshel)
+
+		var lenBytes = utils.IntToBytes(lenth)
+		var msgBytes = utils.IntToBytes(1)
+		var lenInt = len(lenBytes)
+		var msgInt = len(msgBytes)
+
+		var tempBuf = make([]byte, lenth+lenInt+msgInt)
+
+		copy(tempBuf, lenBytes[0:lenInt])
+		copy(tempBuf[lenInt:msgInt+lenInt], msgBytes)
+		copy(tempBuf[lenInt+msgInt:], marshel)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		conn.Write(tempBuf)
+	}
+
+	defer conn.Close()
+}
