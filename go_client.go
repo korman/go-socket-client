7f15d9906e1a2675a43d59e93c48bac0eb977539
korman <korman@qq.com>
2022-10-01 11:28:31 +0800

    Added routes

--- go_client.go
+++ go_client.go
@@ -1,11 +1,20 @@
 package main
 
-import "go_client/network"
+import (
+	"go_client/network"
+	"go_client/routes"
+)
 
 func main() {
 	println("启动客户端连接到服务器")
 
-	err := network.NetworkMgrInstance().StartServer("", "")
+	err := routes.InitRoutes()
+
+	if err != nil {
+		println(err)
+	}
+
+	err = network.NetworkMgrInstance().StartServer("", "")
 
 	if err != nil {
 		println(err)
